0fb1a10bc63fff3cf3c1fabfa0a96418f4c35979
Emily Jackson <ejackson@cocc.edu>
2018-12-11 14:30:13 -0800

    p5 2

--- Projects/Programs/p5/second/sketch.js
+++ Projects/Programs/p5/second/sketch.js
@@ -1,10 +1,31 @@
+var rectWidth;
+
 function setup() {
-  createCanvas(480,120);
+  createCanvas(720, 400);
+  noStroke();
+  background(230);
+  rectWidth = width/4;
 }
 
 function draw() {
-  background(204);
-  ellipse(278, -100, 400, 400);
-  ellipse(120, 100, 110, 110);
-  ellipse(412, 60, 18, 18);
+  // keep draw() here to continue looping while waiting for keys
+}
+
+function keyPressed() {
+  var keyIndex = -1;
+  if (key >= 'a' && key <= 'z') {
+    keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
+  }
+  if (keyIndex == -1) {
+    // If it's not a letter key, clear the screen
+    background(230);
+  } else {
+    // It's a letter key, fill a rectangle
+    randFill_r = Math.floor(Math.random() * 255 + 1);
+    randFill_g = Math.floor(Math.random() * 255 + 1);
+    randFill_b = Math.floor(Math.random() * 255 + 1);
+    fill(randFill_r,randFill_g,randFill_b);
+    var x = map(keyIndex, 0, 25, 0, width - rectWidth);
+    rect(x, 0, rectWidth, height);
+  }
 }
