ffa3a1725af94c985712b7f91605552ec94adfa5
Emily Jackson <ejackson@cocc.edu>
2018-12-11 14:15:07 -0800

    p5 #2

--- Projects/Programs/p5/second/sketch.js
+++ Projects/Programs/p5/second/sketch.js
@@ -1,11 +1,11 @@
 function setup() {
-  createCanvas(500,120)
+  createCanvas(480,120)
 }
 
 function draw() {
-  if(mouseisPressed){
-  fill(0);
-} else {
-  fill (300);
+  background(204);
+  arc(90, 60, 80, 80, 0, radians(90));
+  arc(190, 60, 80, 80, 0, radians(270));
+  arc(290, 60, 80, 80, radians(180), radians(450));
+  arc(390, 60, 80, 80, radians(45), radians(225));
 }
-  square(mouseX, mouseY, 100,100,100, 100)
